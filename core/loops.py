4067ae16ceddd78fad9d5a93746131efc67de703
Valdis Saulespurens <valdis.s.coding@gmail.com>
2026-02-25 17:46:17 +0200

    Add loops demonstration script to illustrate while and for loop behavior

---
+++ core/loops.py
@@ -0,0 +1,21 @@
+# let's see how loop values change with different loop types
+
+print("While loop")
+
+i = 0
+while i < 5:
+    print(i)
+    i += 1
+
+print("outside of the loop and i is", i)
+
+print("Done for now")
+
+print("For loop")
+
+for n in range(5):
+    print(n)
+
+print("outside of the loop and n is", n)
+
+print("Done for now")
